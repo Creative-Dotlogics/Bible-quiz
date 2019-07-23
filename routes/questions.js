6644aed69428ea6565b92b301815ed201a14873a
Creative-Dotlogics <creativedotlogics@gmail.com>
2019-07-23 18:21:18 +0000

    Questions and answers restructured for the /api route

--- routes/questions.js
+++ routes/questions.js
@@ -9,12 +9,57 @@ router.get('/', function(req, res, next) {
 
 router.get('/questions', function(req, res, next) {
   res.json([
-    {"question":"In what city was Jesus born? ","options":["Samaria","Bethlehem","Chicago","Accra"],"answer":1},
-    {"question":"How many books are in the New Testament? ","options":["27","23","37","66"],"answer":0},
-    {"question":"What type of insect did John the Baptist eat in the desert?","options":["Locusts","Grasshopper","Serpent","Snails"],"answer":0},
-    {"question":"Who were the first apostles called to follow Jesus? ","options":["James and John","Peter and Felicia","Peter and Andrew","John and Matthew"],"answer":2},
-    {"question":"How many people did Jesus feed with five loaves of bread and two fish? ","options":["about 100 men","about 6000 men","about 50000 men","about 5000 men"],"answer":3},
-  ]);
+    {
+        "question": "In what city was Jesus born? ",
+        "options": [
+            {"text":"Samaria","label":"A","selected":false,"correct":false},
+            {"text":"Bethlehem","label":"B","selected":false,"correct":false},
+            {"text":"Chicago","label":"C","selected":false,"correct":false},
+            {"text":"Accra","label":"D","selected":false,"correct":false}
+        ],
+        "answer": 1
+    },
+    {
+        "question": "How many books are in the New Testament? ",
+        "options": [
+            {"text":"27","label":"A","selected":false,"correct":false},
+            {"text":"23","label":"B","selected":false,"correct":false},
+            {"text":"37","label":"C","selected":false,"correct":false},
+            {"text":"66","label":"D","selected":false,"correct":false}
+        ],
+        "answer": 0
+    },
+    {
+        "question": "What type of insect did John the Baptist eat in the desert?",
+        "options": [
+            {"text":"Locusts","label":"A","selected":false,"correct":false},
+            {"text":"Grasshopper","label":"B","selected":false,"correct":false},
+            {"text":"Serpent","label":"C","selected":false,"correct":false},
+            {"text":"Snails","label":"D","selected":false,"correct":false}
+        ],
+        "answer": 0
+    },
+    {
+        "question": "Who were the first apostles called to follow Jesus? ",
+        "options": [
+            {"text":"LocuJames and Johnsts","label":"A","selected":false,"correct":false},
+            {"text":"Peter and Felicia","label":"B","selected":false,"correct":false},
+            {"text":"Peter and Andrew","label":"C","selected":false,"correct":false},
+            {"text":"John and Matthew","label":"D","selected":false,"correct":false}
+        ],
+        "answer": 2
+    },
+    {
+        "question": "How many people did Jesus feed with five loaves of bread and two fish? ",
+        "options": [
+            {"text":"about 100 men","label":"A","selected":false,"correct":false},
+            {"text":"about 6000 men","label":"B","selected":false,"correct":false},
+            {"text":"about 50000 men","label":"C","selected":false,"correct":false},
+            {"text":"about 5000 men","label":"D","selected":false,"correct":false}
+        ],
+        "answer": 3
+    }
+]);
 });
 
 module.exports = router;
